4db36907902eae102c6740963279cae54afe163b
Marcus <demaroar@gmail.com>
2020-11-11 22:10:19 -0300

    basic client server comunication

---
+++ clientServer/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+app.get('/:name', (req, res) => {
+    let name = req.params.name;
+
+    res.json({
+        message: `Hello ${name}`
+    });
+});
+
+app.listen(2020, () => {
+    console.log('server is listening on port 2020');
+});
